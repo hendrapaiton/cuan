ab9729e5b65fe2b18022c4159ac075408471104b
Hendra Dwi Saputra <hendra@usa.com>
2024-04-29 07:35:30 +0700

    Item unit testing (bun test)

---
+++ src/__test__/item.test.ts
@@ -0,0 +1,56 @@
+import { beforeEach, afterEach, expect, test } from "bun:test";
+import { PrismaClient, item_jenis } from "@prisma/client";
+
+
+const prisma = new PrismaClient
+
+beforeEach(async () => {
+    const item = await prisma.item.create({
+        data: {
+            kode: "234567",
+            nama: "Pakan Kucing Bolt Ikan Ungu 800gr",
+            jenis: item_jenis.kucing
+        }
+    });
+});
+
+afterEach(async () => {
+    console.log("After Each")
+    await prisma.item.deleteMany({});
+});
+
+// Create Item
+test("Add new item", async () => {
+    const item = await prisma.item.create({
+        data: {
+            kode: "234568",
+            nama: "Pakan Anjing Dog Choize Beef 800gr",
+            jenis: item_jenis.anjing
+        }
+    });
+    expect(item.id).toBeDefined();
+});
+
+// Read Item
+test("Get all item", async () => {
+    const items = await prisma.item.findMany();
+    expect(items.length).toBe(1);
+});
+
+// Update Item
+test("Update an item", async () => {
+    const newNama = "Pakan Anjing Dog Choize Beef Biru 800gr"
+    const updatedItem = await prisma.item.update({
+        where: { kode: "234567" },
+        data: { nama: newNama }
+    });
+    expect(updatedItem.nama).toEqual(newNama);
+});
+
+// Delete Item
+test("Delete an item", async () => {
+    const deletedItem = await prisma.item.delete({
+        where: { kode: "234567" }
+    });
+    expect(deletedItem).toBeDefined();
+});
